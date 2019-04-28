21fef2911cac46c4e5d40867d77e37da93d6c0a0
oszpac <oszpac@126.com>
2019-04-28 09:44:38 +0800

    1.09修改

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,3 +1,4 @@
+
 var app = getApp();
 var that;
 Page({
@@ -8,29 +9,58 @@ Page({
         areas:['广西','福建','众慧'],//可选区域
         selectedAreaId:'',//已选区域id
         selectedArea:'',//已选区域名称
-        proGroup:[],//根据区域关联的产品组
-        groupList:[['A1','A2','A3','A4','B1','B2','SanMa','HengXinA','Special','HengXinB'],['A1','A2','A3','B1',],['C1','C2','C3','C4',]],//区域对应的产品组
+        pingtaiArray: [],//平台数组
+        pingtailist: [['广西新车', '广西二手车', '贵州新车', '贵州二手车'], ['贵州新车', '贵州二手车'], ['众慧乘用车', '众慧车易融']
+        ],
+        selectedPingtai: '',//选择放款平台
+        interestArray: [
+        { "name": '广西新车', "date": '24', 'interest': '7.00%' },
+        { "name": '广西新车', "date": '36', 'interest': '9.50%' },
+        { "name": '广西二手车', "date": '24', 'interest': '7.00%' },
+        { "name": '广西二手车', "date": '36', 'interest': '9.50%' },
+        { "name": '贵州新车', "date": '24', 'interest': '7.00%' },
+        { "name": '贵州新车', "date": '36', 'interest': '9.30%' },
+        { "name": '贵州二手车', "date": '24', 'interest': '7.00%' },
+        { "name": '贵州二手车', "date": '36', 'interest': '9.30%' },
+        { "name": '众慧乘用车', "date": '24', 'interest': '14.33%' },
+        { "name": '众慧乘用车', "date": '36', 'interest': '21.65%' },
+        { "name": '众慧车易融', "date": '24', 'interest': '14.89%' },
+        { "name": '众慧车易融', "date": '36', 'interest': '22.53%' },
+        ],//底息
+      proGroup: [],//产品组
+      gxGroup: [
+        { "name": '广西新车', 'list': ['A1', 'A2', 'A3', 'A4', 'SanMa', 'HengXinA', 'Special']},
+        { "name": '广西二手车', 'list': ['B1', 'B2', 'HengXinB']},
+        { "name": '贵州新车', 'list': ['A1', 'A2', 'A3', 'A4']},
+        { "name": '贵州二手车', 'list': ['B2']},
+      ],
+      fjGroup: [
+        { "name": '贵州新车', 'list': ['A1', 'A2', 'A3']},
+        { "name": '贵州二手车', 'list': ['B1']},
+      ],
+      zhGroup: [
+        { "name": '众慧乘用车', 'list': ['C1', 'C2', 'C3']},
+        { "name": '众慧车易融', 'list': ['C4']},
+      ],//产品数组
         selectedGroupId:'',//已选产品组id
         selectedGroup:'',//已选产品组
-        dateType:['24','36'],//贷款期数
+        dateType:['36','24'],//贷款期数
         selectedDateId:'',//已选贷款期数id
         selectedDate:'',//已选贷款期数
-		theftSchemeArray:['现金','加融','赠送'],//盗抢险方案
-		selectedTheftSchemeId:'',//已选盗抢险方案id
+      theftSchemeArray: ['加融','现金','赠送'],//盗抢险方案
+		  selectedTheftSchemeId:'',//已选盗抢险方案id
         selectedTheftScheme:'',//已选盗抢险方案
         theftAcountList:[],//盗抢可选金额
 		theftAcount:0,//盗抢险金额
         gx:[
-        {"name":'A1',"date":'24','list':['10.22%', '11.29%', '12.36%', '13.43%' ]},
-        {"name":'A1',"date":'36','list':['12.80%', '13.89%', '14.99%', '16.08%' ]},
+        {"name":'A1',"date":'24','list':['11.82%', '12.89%', '13.96%', '15.03%' ]},
+        {"name":'A1',"date":'36','list':['14.43%', '15.52%', '16.62%', '17.71%' ]},
         {"name":'A2',"date":'24','list':['12.14%', '13.21%', '14.28%', '15.35%','16.42%', '17.49%', '18.56%' ]},
         {"name":'A2',"date":'36','list':['14.76%', '15.85%', '16.95%', '18.04%','19.14%', '20.23%', '21.33%' ]},
-        {"name":'A3',"date":'24','list':['12.18%', '13.25%', '14.32%', '15.39%','17.70%','18.77%','19.84%' ]},
-        {"name":'A3',"date":'36','list':['15.90%','16.07%', '16.99%', '18.08%','20.45%', '21.55%', '22.64%' ]},
-        {"name":'A4',"date":'24','list':['15.52%', '16.59%', '17.66%' ]},
-        {"name":'A4',"date":'36','list':['16.62%', '19.09%', '20.19%' ]},
-        {"name":'B1',"date":'24','list':['13.96%', '15.03%', '16.10%', '17.17%' ]},
-        {"name":'B1',"date":'36','list':['16.62%', '17.72%', '18.81%', '19.91%' ]},
+        {"name":'A3',"date":'24','list':['13.42%', '14.49%', '15.56%', '16.63%','17.70%','18.77%' ]},
+        {"name":'A3',"date":'36','list':['16.07%', '17.71%', '18.26%','19.36%', '20.45%', '21.55%' ]},
+        {"name":'A4',"date":'24','list':['14.17%', '15.24%', '16.31%' ]},
+        {"name":'A4',"date":'36','list':['16.62%', '17.72%', '18.81%' ]},
         {"name":'B2',"date":'24','list':['15.03%', '16.10%', '17.17%', '18.24%','19.31%','20.38%','21.45%','22.52%' ]},
         {"name":'B2',"date":'36','list':['17.71%', '18.81%', '19.90%', '21.00%','22.09%', '23.19%', '24.28%', '25.38%' ]},
         {"name":'SanMa',"date":'24','list':['15.75%', '16.63%']},
@@ -47,9 +77,9 @@ Page({
         {"name":'A2',"date":'24','list':['11.82%', '12.89%', '13.96%', '15.03%','16.10%', '17.17%', '18.24%' ]},
         {"name":'A2',"date":'36','list':['14.22%', '15.31%', '16.40%', '17.50%','18.59%', '19.68%', '20.78%' ]},
         {"name":'A3',"date":'24','list':['14.17%', '15.24%', '16.31%', '17.38%' ]},
-        {"name":'A3',"date":'36','list':['16.62%', '17.72%', '18.81%', '19.90%' ]},
-        {"name":'B1',"date":'24','list':['13.42%', '14.49%', '15.56%', '16.63%','17.70%', '18.77%', '19.84%', '20.91%', '21.98%']},
-        {"name":'B1',"date":'36','list':['16.62%', '17.72%', '18.81%', '19.90%','20.23%', '21.32%', '22.42%','23.51%','24.60%' ]}
+        {"name":'A3',"date":'36','list':['16.62%', '17.72%', '18.81%']},
+        {"name":'B1',"date":'24','list':['13.07%', '14.14%', '15.21%', '16.28%','17.35%', '18.42%', '19.49%', '20.56%', '21.63%']},
+        {"name":'B1',"date":'36','list':['15.50%', '16.59%', '17.68%', '18.78%','19.87%', '20.96%', '22.06%','23.15%','24.24%' ]}
         ],
         zonghui:[
         {"name":'C1',"date":'24','list':['22.28%']},
@@ -63,26 +93,7 @@ Page({
         ],
         rateArray:[],//产品费率数组
         productRate:'',//产品费率
-        pingtaiArray:[],//放款平台数组
-		otherPingtaiArray:['广西新车','广西二手车','贵州新车','贵州二手车'],//其他放款平台数组
-		zonghuiPingtaiArray:['众慧商用车','众慧乘用车','众慧车易融'],//众慧放款平台数组
-        selectedPingtai:'',//选择放款平台
-        interestArray:[
-            {"name":'广西新车',"date":'24','interest':'7%'},
-            {"name":'广西新车',"date":'36','interest':'9.50%'},
-            {"name":'广西二手车',"date":'24','interest':'7%'},
-            {"name":'广西二手车',"date":'36','interest':'9.50%'},
-            {"name":'贵州新车',"date":'24','interest':'7.00%'},
-            {"name":'贵州新车',"date":'36','interest':'9.3%'},
-            {"name":'贵州二手车',"date":'24','interest':'7.00%'},
-            {"name":'贵州二手车',"date":'36','interest':'9.3%'},
-            {"name":'众慧商用车',"date":'24','interest':'17.00%'},
-            {"name":'众慧商用车',"date":'36','interest':'0'},
-            {"name":'众慧乘用车',"date":'24','interest':'14.33%'},
-            {"name":'众慧乘用车',"date":'36','interest':'21.65%'},
-            {"name":'众慧车易融',"date":'24','interest':'14.89%'},
-            {"name":'众慧车易融',"date":'36','interest':'22.53%'},
-        ],//底息
+        
         serviceCharges:'', //综合服务费
         approvalMoney:'', //送审金额（最终贷款金额）
         approvalPercent:'', //送审成数
@@ -128,37 +139,47 @@ Page({
             }
             that.reset();
         }
+        
     },
     //选择区域
     areaChange:(e)=>{
         let selectedAreaId = e.detail.value;
         let selectedArea = that.data.areas[selectedAreaId];
-        let proGroup = [];
-        proGroup = that.data.groupList[selectedAreaId];
-		let pingtaiArray = [];
-		if(selectedAreaId==2){
-			pingtaiArray = that.data.zonghuiPingtaiArray;
-		}else{
-			pingtaiArray = that.data.otherPingtaiArray;
-		}
+        let pingtaiArray = [];
+      pingtaiArray = that.data.pingtailist[selectedAreaId];
+	
         that.setData({
             selectedAreaId:selectedAreaId,
             selectedArea: selectedArea,
-            proGroup:proGroup,
+            pingtaiArray: pingtaiArray,
             rateArray:[],
             productRate:[],
-			pingtaiArray:pingtaiArray
+            selectedPingtai:'',
+            selectedGroup: '', 
+            productRate:'',  
         })
         that.reset();
     },
+  //选择平台
+  pingtaiChange: (e) => {
+    let index = e.detail.value;
+    let selectedPingtai = that.data.pingtaiArray[index];
+    that.setData({
+      selectedPingtai: selectedPingtai,
+      selectedGroup: '',
+      productRate: '', 
+    })
+    that.getproGroup();
+    that.reset();
+  },
     //选择产品组
     proGroupChange:(e)=>{
-        let selectedGroupId = e.detail.value;
-        let selectedGroup = that.data.proGroup[selectedGroupId];
+      let index = e.detail.value;
+      let selectedGroup = that.data.proGroup[index];
         that.setData({
-            selectedGroupId:selectedGroupId,
-            selectedGroup: selectedGroup,
-        })
+             selectedGroup: selectedGroup,
+             productRate: '', 
+         })
         that.getRateArray();
         that.reset();
     },
@@ -211,15 +232,7 @@ Page({
         })
         that.reset();
     },
-     //选择平台
-    pingtaiChange:(e)=>{
-        let index = e.detail.value;
-        let selectedPingtai = that.data.pingtaiArray[index];
-        that.setData({
-            selectedPingtai: selectedPingtai,
-        })
-        that.reset();
-    },
+    
     //计算2%盗抢
     caluPercentTheft:(amountPaid)=>{
         let percentTheft = that.hundredCeil(amountPaid*2/100);
@@ -246,45 +259,51 @@ Page({
     * @param    {[type]}   interes [底息]
     * @return   {[type]}   [description]
     */
-    caluServiceCharges:(theftAcount,interes)=>{
-        let serviceCharges = 0;
-        let amountPaid = that.hundredCeil(that.data.amountPaid);//走款金额
-        let productRate = that.data.productRate;//产品费率
-        let selectedAreaId = that.data.selectedAreaId;//已选区域
-        
-        productRate = parseFloat(productRate);
-        interes = parseFloat(interes);
-       
-        if (productRate==0||isNaN(productRate)||interes==0||isNaN(interes)) {
-            return serviceCharges;
-        }
-        //（产品费率-底息）/（100%+底息）×走款金额 向上百位取整+加融现金盗抢+2%盗抢金额+200
-        let _rate = parseFloat((productRate-interes)/(100+interes));
-        let extra = 0;
-        if (selectedAreaId==='0') {//广西
-            extra = 200;
-        }
-        if (selectedAreaId==='1') {//福建
-            extra = 100;
-        }
-         if (selectedAreaId==='2') {//众慧
-            extra = 0;
-        }
-        serviceCharges = _rate*amountPaid+theftAcount+extra;
-        return that.hundredCeil(parseInt(serviceCharges));
-    },
-    caluInteres:()=>{//获取底息 
-        let selectedPingtai = that.data.selectedPingtai;
-        let selectedDate = that.data.selectedDate;
-        let interestArray = that.data.interestArray;
-        let interes = '';//底息
-        interestArray.forEach(function(v,k){
-            if (v.name==selectedPingtai&&v.date==selectedDate) {
-                interes = v.interest;
-            }
-        });
-        return interes;
-    },
+  caluServiceCharges: (interes) => {
+    let serviceCharges = 0;
+    let amountPaid = that.data.amountPaid;//走款金额
+    let productRate = that.data.productRate;//产品费率
+    let selectedAreaId = that.data.selectedAreaId;//已选区域
+    let theftAcount=that.data.theftAcount//盗抢金额
+
+    amountPaid = parseInt(amountPaid);
+    productRate = parseFloat(productRate);
+    interes = parseFloat(interes);
+    theftAcount = parseInt(theftAcount);
+
+    if (productRate == 0 || isNaN(productRate) || interes == 0 || isNaN(interes)) {
+      return serviceCharges;
+    }
+
+    //（产品费率-底息）/（100%+底息）×走款金额 向上百位取整+盗抢金额+200
+    let _rate = (productRate/100-interes/100)/(1+interes/100);
+    let extra = 0;
+    if (selectedAreaId === '0') {//广西
+      extra = 200;
+    }
+    if (selectedAreaId === '1') {//福建
+      extra = 100;
+    }
+    if (selectedAreaId === '2') {//众慧
+      extra = 0;
+    }
+    serviceCharges = _rate * amountPaid + extra
+    return that.hundredCeil(parseInt(serviceCharges));
+  },
+   
+  caluInteres: () => {//获取底息 
+    let selectedPingtai = that.data.selectedPingtai;
+    let selectedDate = that.data.selectedDate;
+    let interestArray = that.data.interestArray;
+    let interes = '';//底息
+    interestArray.forEach(function (v, k) {
+      if (v.name == selectedPingtai && v.date == selectedDate) {
+        interes = v.interest;
+      }
+    });
+    return interes;
+  },
+   
     //百位取整
     hundredCeil:(inp)=>{
         let input = parseFloat(inp);
@@ -296,6 +315,7 @@ Page({
         }else{
             return inp ;
         }
+
     },
     getRateArray:()=>{
         let selectedAreaId = that.data.selectedAreaId;
@@ -313,6 +333,7 @@ Page({
             if (selectedAreaId==='2') {
                 area = that.data.zonghui;
             }
+
             area.forEach(function(v,k){
                 if(v.name==selectedGroup&&v.date==selectedDate){
                     rateArray = v.list;           
@@ -320,12 +341,42 @@ Page({
            
             });
         }
+
         that.setData({
             rateArray :rateArray,
             productRate:''
         });
 
     },
+  getproGroup: () => {
+    let selectedAreaId = that.data.selectedAreaId;
+    let selectedPingtai = that.data.selectedPingtai;
+    let proGroup= [];
+    if (selectedAreaId != '' && selectedPingtai != '') {
+      let pro;
+      if (selectedAreaId === '0') {
+        pro = that.data.gxGroup;
+      }
+      if (selectedAreaId === '1') {
+        pro = that.data.fjGroup;
+      }
+      if (selectedAreaId === '2') {
+        pro = that.data.zhGroup;
+      }
+
+    pro.forEach(function (v, k) {
+      if (v.name == selectedPingtai) {
+        proGroup = v.list;
+      }
+
+    });
+  }
+    that.setData({
+      proGroup: proGroup,
+      selectedGroup: ''
+    });
+
+  },
 	getTheftAcountList:()=>{
 		let amountPaid = parseInt(that.data.amountPaid);
 		if(amountPaid==0 ||isNaN(amountPaid)){
@@ -363,7 +414,7 @@ Page({
             selectedAreaId:'',//已选区域id
             selectedArea:'',//已选区域名称
             proGroup:[],//根据区域关联的产品组
-			pingtaiArray:[],
+			      pingtaiArray:[],
             selectedGroupId:'',//已选产品组id
             selectedGroup:'',//已选产品组
             selectedDateId:'',//已选贷款期数id
@@ -411,7 +462,10 @@ Page({
             that.showModal('请选择区域');
             return false;
         }  
-        
+        if (selectedPingtai == '') {
+        that.showModal('请选择车辆类型');
+            return false;
+        } 
         if (selectedGroup=='') {
             that.showModal('请选择产品组');
             return false;
@@ -435,32 +489,24 @@ Page({
             that.showModal('请选择产品费率');
             return false;
         } 
-        if (selectedPingtai=='') {
-            that.showModal('车辆类型');
-            return false;
-        } 
-
-        if (selectedPingtai=='众慧商用车'&&selectedDate=='36') {
-            that.showModal('请众慧商用车平台未提供36期贷款服务');
-            return false;
-        } 
+       
 
         //计算贷款成数
         let paidPercent = that.caluAmountPaidPercent();
         
         //计算底息,结果带百分号
         let interes = that.caluInteres();
-        //计算综合服务费-担保费
-        let serviceCharges = that.caluServiceCharges(theftAcount,interes);
-        //计算送审金额（最终贷款金额）
-		let approvalMoney = 0;
-		//['现金','加融','赠送']
-		if(selectedTheftSchemeId==='1'||selectedTheftSchemeId==='2'){
-			approvalMoney = that.hundredCeil(parseInt(amountPaid))+parseInt(serviceCharges); 
-		}
-		if(selectedTheftSchemeId==='0'){
-			approvalMoney = that.hundredCeil(parseInt(amountPaid))+parseInt(serviceCharges)-parseInt(theftAcount); 
-		}
+        //计算综合服务费
+      let serviceCharges =0;
+      if (selectedTheftSchemeId === '0' ){
+        serviceCharges = that.caluServiceCharges(interes) + theftAcount;
+      } else { serviceCharges = that.caluServiceCharges(interes);
+      }
+       
+        //计算送审金额（最终贷款金额） =送审金额+综合服务费
+        let approvalMoney = that.hundredCeil(parseInt(amountPaid)) + parseInt(serviceCharges)
+    
+		
           //计算送审成数
         let approvalPercent = Math.round(approvalMoney/chejia * 10000) / 100.00+"%" ;
 		//还款总额
@@ -491,4 +537,5 @@ Page({
            
         });
     }
+
 })
